55b4c690b1267f9c9fd2f6844c4f99d3e94a7b39
MohamedSsherif <mohamedsheriffouad27gmail.com>
2024-08-12 17:10:02 +0300

    create app styles

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 const kPrimaryColor= Color.fromARGB(255, 20, 3, 20);
 const kTransitionDuration = Duration(milliseconds: 300);
 const testImage = 'assets/images/test_image.png';
+const kGtSectraFine =  'GT Sectra Fine';
--- lib/feature/home/presentation/views/widgets/home_view_body.dart
+++ lib/feature/home/presentation/views/widgets/home_view_body.dart
@@ -1,29 +1,47 @@
+import 'package:booklyapp/constants.dart';
+import 'package:booklyapp/core/utils/assets.dart';
 import 'package:booklyapp/core/utils/styles.dart';
 import 'package:booklyapp/feature/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:booklyapp/feature/home/presentation/views/widgets/featured_list_view.dart';
 import 'package:flutter/material.dart';
-
 
 class HomeBody extends StatelessWidget {
   const HomeBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  const Column(
+    return const Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         CustomAppBar(),
-         FeaturedBooksListView(),
-         Padding(
-           padding: EdgeInsets.only(top: 40, left: 20),
-           child: Text(
+        FeaturedBooksListView(),
+        Padding(
+          padding: EdgeInsets.only(top: 40, left: 20),
+          child: Text(
             'Best Seller',
-            style: Styles.titleMedium,
-           ),
-         )
-      
+            style: Styles.textStyle18,
+          ),
+        ),
+        BestSellerListViewItem(),
       ],
     );
   }
 }
 
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Image.asset(
+          testImage,
+          width: MediaQuery.of(context).size.width * 0.40,
+          height: MediaQuery.of(context).size.height * 0.23,
+        ),
+       
+      ],
+    );
+  }
+}
