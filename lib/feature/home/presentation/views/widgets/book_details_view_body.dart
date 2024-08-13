5c72a43a9b66469f5ffecf49bcc07f79562aae75
MohamedSsherif <mohamedsheriffouad27gmail.com>
2024-08-13 18:48:08 +0300

    add text and subtitle to detailes

--- lib/feature/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/feature/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:booklyapp/core/utils/styles.dart';
+import 'package:booklyapp/feature/home/presentation/views/widgets/book_rating.dart';
 import 'package:booklyapp/feature/home/presentation/views/widgets/custom_books_details_appbar.dart';
 import 'package:booklyapp/feature/home/presentation/views/widgets/custom_list_home_view.dart';
 import 'package:flutter/material.dart';
@@ -7,15 +9,29 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 30),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
+        //mainAxisAlignment: MainAxisAlignment.start,
+        //crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          CustomBookDetailsAppBar(),
-          Padding(
-            padding: EdgeInsets.all(24.0),
+          const CustomBookDetailsAppBar(),
+          const Padding(
+            padding: EdgeInsets.only(top: 24.0),
             child: CustomBookImage(),
           ),
+          const SizedBox(height: 30),
+          const Text(
+            'The Jungle Book',
+            style: Styles.textStyle30,
+          ),
+          const SizedBox(height: 6),
+          Text('Rudyard Kipling',
+              style: Styles.textStyle18
+                  .copyWith(
+                    color: Colors.grey,
+                     fontStyle: FontStyle.italic),),
+          const BookRating(),
         ],
       ),
     );
