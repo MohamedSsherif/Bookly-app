270d3053f55fe6efe7a49ffb4626bd604e19fa44
MohamedSsherif <mohamedsheriffouad27gmail.com>
2024-08-13 21:32:29 +0300

    create books action

--- lib/feature/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/feature/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,5 +1,7 @@
 import 'package:booklyapp/core/utils/styles.dart';
+import 'package:booklyapp/core/widgets/custom_button.dart';
 import 'package:booklyapp/feature/home/presentation/views/widgets/book_rating.dart';
+import 'package:booklyapp/feature/home/presentation/views/widgets/books_action.dart';
 import 'package:booklyapp/feature/home/presentation/views/widgets/custom_books_details_appbar.dart';
 import 'package:booklyapp/feature/home/presentation/views/widgets/custom_list_home_view.dart';
 import 'package:flutter/material.dart';
@@ -34,8 +36,11 @@ class BookDetailsViewBody extends StatelessWidget {
           const BookRating(
             mainAxisAlignment: MainAxisAlignment.center,
           ),
+          const SizedBox(height: 37),
+          const BookAction()
         ],
       ),
     );
   }
 }
+
