e88303dd9bb1d0121b174ac3e86a8481102fa261
MohamedSsherif <mohamedsheriffouad27gmail.com>
2024-08-15 18:10:09 +0300

    search view done

--- lib/feature/search/presentation/views/widgets/search_view_body.dart
+++ lib/feature/search/presentation/views/widgets/search_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:booklyapp/core/utils/styles.dart';
+import 'package:booklyapp/feature/home/presentation/views/widgets/book_list_view_item.dart';
 import 'package:booklyapp/feature/search/presentation/views/widgets/custom_search_text_field.dart';
 import 'package:flutter/material.dart';
 
@@ -9,16 +11,36 @@ class SearchViewBody extends StatelessWidget {
     return const Scaffold(
       body: SafeArea(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
            CustomSearchTextField(),
-             Expanded(
-              child: Center(
-                child: Text('Search Results'),
-              ),
-            ),
+           Padding(
+             padding: EdgeInsets.all(12.0),
+             child: Text('search result',style: Styles.textStyle16,),
+           ),
+            Expanded(child: SearchResultListView())
           ],
         ),
       ),
+    );
+  }
+}
+
+class SearchResultListView extends StatelessWidget {
+  const SearchResultListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: EdgeInsets.zero,
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return const Padding(
+          padding: EdgeInsets.symmetric(vertical: 10),
+          child: BookListViewItem(),
+        );
+      },
     );
   }
 }
